d4dfb99bafe94107c5e29d6ae08588931d28e5c5
unknown <vrameshkumar260@gmail.com>
2024-06-12 10:49:37 +0530

    second

--- routers/stack.py
+++ routers/stack.py
@@ -1,57 +1,53 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 # database
-from databases.database import *
-#model
-from models import *
-#schema
-from schemas import *
-#security
+from databases.database import get_db
+# model
+from models import m_stack
+# schema
+from schemas import s_stack
+# security
 from security import VerifyRoute
+
 stack_router = APIRouter(
     prefix="/api",
     tags=["Stack"],
     route_class=VerifyRoute
 )
 
-
-
-@stack_router.get("/api/stack")
-def stack_show(db : Session = Depends(get_db)):
-    
+@stack_router.get("/stack")
+def stack_show(db: Session = Depends(get_db)):
     # Getting all stack
-    
     stack = db.query(m_stack.Stack).all()
     
     if stack:
         return {
-            'status' : 'success',
-            'msg': 'Successfully Stack Added',
-            'data' : stack
+            'status': 'success',
+            'msg': 'Successfully retrieved stack',
+            'data': stack
         }
     else:
         return {
-            'status' : 'failed',
-            'msg' : 'Empty Stack'
+            'status': 'failed',
+            'msg': 'Empty stack'
         }
-
 
 # CUSTOM RESPONSE
-@stack_router.post("/api/stack",response_model=s_stack.Stackcustom)
-def creating_stack(req : s_stack.Stack,db : Session = Depends(get_db)):
-    
-    # Addting Stack with custom response
-    
+@stack_router.post("/stack")
+def creating_stack(req: s_stack.Stack, db: Session = Depends(get_db)):
+    # Adding Stack with custom response
     try:
-    
         stack = m_stack.Stack(**req.dict())
-        
         db.add(stack)
         db.commit()
+        db.refresh(stack)  # Refresh the instance to get the updated data
         
-        return stack
-    except:
         return {
-            'error' : 'error '
+            'status': 'success',
+            'msg': 'Successfully added',
+            'data': stack.__dict__  # Including the stack data
         }
-    
+    except SQLAlchemyError as e:
+        db.rollback()  # Rollback the transaction in case of an error
+        raise HTTPException(status_code=400, detail=f"Unable to add stack. Error: {str(e)}")
